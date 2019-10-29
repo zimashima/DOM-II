35a1f07c2644bfc1c408ff0e5903f93f3628cbb3
Mashima <52222646+zimashima@users.noreply.github.com>
2019-10-29 16:59:16 -0400

    first commit, 3 interactive contents added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+//body
+
+const theBody = document.querySelector('body')
+
+theBody.addEventListener('dblclick', (e)=> {
+    alert(`Seriously? What's the point of doubleclicking this?`);
+})
+//nav
+
+//intro
+
+//content
+
+//destinations
+
+const destinations = document.querySelectorAll('.destination');
+
+destinations.forEach( place => {
+    place.addEventListener('mouseover', (e) => {
+        place.style.backgroundColor = 'whitesmoke';
+    })
+})
+
+destinations.forEach( place => {
+    place.addEventListener('mouseleave', (e) => {
+        place.style.backgroundColor = 'transparent';
+    })
+})
